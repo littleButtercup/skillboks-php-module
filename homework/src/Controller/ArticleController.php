40a2f970ce0574109a422e7f81cd5b9fbf8a3ef9
Sergey <seriaa4@gmail.com>
2023-05-08 02:20:05 +0500

    реализовал изиенения и добавления слов в текст

--- homework/src/Controller/ArticleController.php
+++ homework/src/Controller/ArticleController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Command\ArticleContentProviderCommand;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use Symfony\Component\Routing\Annotation\Route;
+use Michelf\Markdown;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 class ArticleController extends AbstractController
 {
@@ -19,9 +23,23 @@ class ArticleController extends AbstractController
     /**
      * @Route("/articles/view/{id}", name="app_detailse")
      */
-    public function viewpage(string $id, ArticleProvider $articleProvider)
+    public function viewpage(string $id, ArticleContentProvider $articleContentProvider, ArticleProvider $articleProvider, ArticleContentProviderCommand $command)
     {
+        $arrayWords = ['MEGAWORD', 'HYPERWORD', 'UBERWORD', ' COOLWORD', 'DUPERWORD'];
+        $randon = rand(1,10);
+        $word = $this->getParameter('app.words_bold') ? '**'.$arrayWords[rand(0,4)].'**':'*'.$arrayWords[rand(0,4)].'*';
+        $randomWord = $randon <= 7 ? $word : " ";
+        $randomQuantity = $randon <= 7 ? $randon : 0;
+        $cache = new FilesystemAdapter();
+        $item = $cache->getItem('markdown_' . md5($id));
+
+//        if (!$item->isHit()){
+        if (true){
+            $item->set(Markdown::defaultTransform($articleContentProvider->get(rand(1,4), $randomWord, $randomQuantity)));
+            $cache->save($item);
+        }
+        $articleContent = $item->get();
         $input = $articleProvider->article();
-        return $this->render('articles/view.html.twig', ['article' => $input]);
+        return $this->render('articles/view.html.twig', ['article' => $input, 'articleContent' => $articleContent]);
     }
 }
